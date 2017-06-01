fe2f790d7bec6b9afe922d1dcbb1a4dabf9fe9e4
Walter A. Boring IV <waboring@hemna.com>
2017-06-01 09:45:20 -0700

    Added rage, magic, covfefe and randomized hello

--- tableflip.py
+++ tableflip.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import random
+
 import sopel.module
 
 
@@ -8,21 +10,87 @@ def flip_help(bot, trigger):
     flip = "(╯°□°)╯︵ ┻━┻ ".decode("utf-8")
     bot.say("Welcome to Table flipping bot.")
     bot.say(flip)
-    bot.say("@!           - Standard table flip.")
+    bot.say("@!           - Hello.")
     bot.say("@!b <nick>   - Battle another nick table flip.")
+    bot.say("@!c          - Covfefe table flip.")
     bot.say("@!d <nick>   - Battle win another nick table flip.")
     bot.say("@!f          - Fat table flip.")
+    bot.say("@!g          - The Finger table flip.")
     bot.say("@!h          - Hercules table flip.")
     bot.say("@!j          - Jedi table flip.")
+    bot.say("@!m          - Magic table flip.")
+    bot.say("@!r          - Rage table flip.")
+    bot.say("@!t          - Standard table flip.")
     bot.say("@!z          - Bear table flip.")
 
 
 # matching exactly @!
 @sopel.module.rule('\@\!$')
 def flip(bot, trigger):
-    flip = "(╯°□°)╯︵ ┻━┻ ".decode("utf-8")
+    hi_list = [
+        "( ･ω･)ﾉ",
+        "( ^_^)／",
+        "(^o^)/",
+        "( ´ ▽ ` )ﾉ",
+        "(=ﾟωﾟ)ﾉ",
+        "( ・_・)ノ",
+        "(。･∀･)ﾉ",
+        "(*ﾟ͠ ∀ ͠)ﾉ",
+        "(♦亝д 亝)ﾉ",
+        "( *՞ਊ՞*)ﾉ",
+        "(｡Ő▽Ő｡)ﾉﾞ",
+        "(ஐ╹◡╹)ノ",
+        "(✧∇✧)╯",
+        "(^▽^)/ ʸᵉᔆᵎ",
+        "(。･д･)ﾉﾞ",
+        "(◍˃̶ᗜ˂̶◍)ﾉ”",
+        "(*´･д･)ﾉ",
+        "|。･ω･|ﾉ",
+        "(ه’́⌣’̀ه )／",
+        "ヽ(´･ω･`)､",
+        "ヘ(°￢°)ノ",
+        "＼(-_- )",
+        "(¬_¬)ﾉ",
+        "(;-_-)ノ",
+        "(^-^*)/",
+        "＼( ･_･)",
+        "ヾ(-_-;)",
+        "|ʘ‿ʘ)╯"
+    ]
+
+    index = random.randint(0, len(hi_list)-1)
+
+    flip = hi_list[index].decode("utf-8")
     flip1 = ('%s %s' % (trigger.nick, flip))
     bot.say(flip1)
+
+
+# matching @!b
+# also appends anything after @!b to end of message
+@sopel.module.rule('\@\!b')
+def flip_battle(bot, trigger):
+    flip = "(╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯)".decode("utf-8")
+    flip1 = ('%s %s' % (trigger.nick, flip))
+    other = str(trigger).replace('@!b', '')
+    bot.say(flip1 + other)
+
+
+# matching @!c
+@sopel.module.rule('\@\!c')
+def flip_covfefe(bot, trigger):
+    flip = "༼ノಠل͟ಠ༽ノ ︵ ┻━┻".decode("utf-8")
+    flip1 = ('%s %s' % (trigger.nick, flip))
+    bot.say(flip1)
+
+
+# matching @!d
+# also appends anything after @!d to end of message
+@sopel.module.rule('\@\!d')
+def flip_dude(bot, trigger):
+    flip = "(╯°Д°）╯︵ /(.□ . )".decode("utf-8")
+    flip1 = ('%s %s' % (trigger.nick, flip))
+    other = str(trigger).replace('@!d', '')
+    bot.say(flip1 + other)
 
 
 # matching @!f
@@ -33,10 +101,10 @@ def flip_fat(bot, trigger):
     bot.say(flip1)
 
 
-# matching @!z
-@sopel.module.rule('\@\!z')
-def flip_bear(bot, trigger):
-    flip = "ʕノ•ᴥ•ʔノ ︵ ┻━┻".decode("utf-8")
+# matching @!g
+@sopel.module.rule('\@\!g')
+def flip_finger(bot, trigger):
+    flip = "╭∩╮◕ل͜◕)╭∩╮  ︵┻┻".decode("utf-8")
     flip1 = ('%s %s' % (trigger.nick, flip))
     bot.say(flip1)
 
@@ -57,21 +125,33 @@ def flip_jedi(bot, trigger):
     bot.say(flip1)
 
 
-# matching @!d
-# also appends anything after @!d to end of message
-@sopel.module.rule('\@\!d')
-def flip_dude(bot, trigger):
-    flip = "(╯°Д°）╯︵ /(.□ . )".decode("utf-8")
+# matching @!m
+@sopel.module.rule('\@\!m')
+def flip_magic(bot, trigger):
+    flip = "༼∩ຈل͜ຈ༽つ━☆ﾟ.*･｡ﾟ ︵ ┻━┻".decode("utf-8")
     flip1 = ('%s %s' % (trigger.nick, flip))
-    other = str(trigger).replace('@!d', '')
-    bot.say(flip1 + other)
+    bot.say(flip1)
 
 
-# matching @!b
-# also appends anything after @!b to end of message
-@sopel.module.rule('\@\!b')
-def flip_battle(bot, trigger):
-    flip = "(╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯)".decode("utf-8")
+# matching @!r
+@sopel.module.rule('\@\!r')
+def flip_rage(bot, trigger):
+    flip = "(ノಠ益ಠ)ノ彡┻━┻".decode("utf-8")
     flip1 = ('%s %s' % (trigger.nick, flip))
-    other = str(trigger).replace('@!b', '')
-    bot.say(flip1 + other)
+    bot.say(flip1)
+
+
+# matching @!t
+@sopel.module.rule('\@\!t')
+def flip_table(bot, trigger):
+    flip = "(╯°□°)╯︵ ┻━┻ ".decode("utf-8")
+    flip1 = ('%s %s' % (trigger.nick, flip))
+    bot.say(flip1)
+
+
+# matching @!z
+@sopel.module.rule('\@\!z')
+def flip_bear(bot, trigger):
+    flip = "ʕノ•ᴥ•ʔノ ︵ ┻━┻".decode("utf-8")
+    flip1 = ('%s %s' % (trigger.nick, flip))
+    bot.say(flip1)
